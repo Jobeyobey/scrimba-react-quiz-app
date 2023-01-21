b0a03a419b1ee07ec75bdc7d7ae24fe905607bc4
Jobeyobey <117482381+Jobeyobey@users.noreply.github.com>
2023-01-21 20:08:44 +0000

    Rendering trivia question/answers
    Got TriviaQuestion.js rendering when "Start Quiz" is clicked. Began styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { nanoid } from "nanoid"
 import "./style.css"
 import Menu from "./components/Menu"
 import TriviaQuestion from "./components/TriviaQuestion"
@@ -8,18 +9,68 @@ function App() {
     questions: null,
     loading: false
   })
-  
-  console.log(triviaQuestions);
+
+  /**
+   * The `if` statement and `chosenQuestions` declaration immediately below are needed once the trivia questions have been fetched and the game begins.
+   * First, it creates `questionElements` in global scope.
+   * Then, if the questions have been fetched, map over them and create the <TriviaQuestion /> elements for the app
+   * */ 
+  let questionElements = null;
+  if(triviaQuestions.questions) {
+    let chosenQuestions = triviaQuestions.questions.results;
+    questionElements = chosenQuestions.map((question, index) => {
+      return <TriviaQuestion
+        key = {nanoid()}
+        id = {index}
+        category = {question.category}
+        answer = {question.correct_answer}
+        incorrect_answers = {question.incorrect_answers}
+        question = {question.question}
+        type = {question.type}
+      />
+    })
+  }
 
   return (
     <div className="App">
-      {
+
+      { /* If questions exist, display menu. Otherwise, display trivia questions */
         !triviaQuestions.questions ?
         <Menu triviaQuestions={triviaQuestions} setTriviaQuestions={setTriviaQuestions}/> :
-        <TriviaQuestion />
+        <div>
+          {questionElements}
+        </div>
       }
+
     </div>
   );
 }
 
 export default App;
+
+/**
+ * triviaQuestions.questions is an object looking like this {
+ *  triviaQuestions: {
+ *    questions: {
+ *      results: [
+ *        {
+ *          category: "Category"
+ *          correct_answer: "Correct Answer"
+ *          difficulty: "diff"
+ *          incorrect_answers: [
+ *          1,
+ *          2,
+ *          3
+ *          ]
+ *          question: "Question"
+ *          type: "multiple / true/false"
+ *        },
+ *        object,
+ *        object,
+ *        object,
+ *        object,
+ *      ]
+ *    } 
+ *  }
+ * }
+ */
--- src/components/TriviaQuestion.js
+++ src/components/TriviaQuestion.js
@@ -1,8 +1,37 @@
 import React from "react"
+import { nanoid } from "nanoid"
 
-export default function TriviaQuestion() {
+export default function TriviaQuestion({id, category, answer, incorrect_answers, question, type}) {
+
+    // Function to shuffle an array
+    function shuffle(array) {
+        let currentIndex = array.length;
+        let randomIndex;
+
+        while(currentIndex > 0) {
+            randomIndex = Math.floor(Math.random() * array.length);
+            currentIndex--;
+
+            [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+        }
+
+        return array;
+    }
+
+    // Shuffle the answer with the incorrect answers
+    const possibleAnswers = shuffle([answer, ...incorrect_answers])
+
+    let answerElements = possibleAnswers.map((answer) => {
+        return <button className="question--answers" key={nanoid()}>{answer}</button>
+    })
 
     return (
-        <h1>Hello, this is a question</h1>
+        <div>
+            <h2 className="question--title">{id + 1}: {question}</h2>
+            <div className="question--answers-container">
+                {answerElements}
+            </div>
+            <hr className="question--divider"></hr>
+        </div>
     )
 }
